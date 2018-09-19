6e4b7b443743e5e60e36e3dc594a334ff551b4ae
nebulak <nebulak@nebulak.localhost>
2018-09-19 11:23:40 +0200

    init

---
+++ main.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+
+import sys, threading, time, os
+"""
+from stem.control import Controller
+from stem import SocketError, UnsatisfiableRequest
+import stem.process
+from stem.util import term
+from flask import Flask
+from flask import send_from_directory
+"""
+# crypto
+import pyblake2
+import pylibscrypt
+import ed25519
+import base58
+
+import socks
+
+# Stem for hidden services: source: https://stem.torproject.org/tutorials/over_the_river.html
+# Serving static files with flask: source: https://www.techcoil.com/blog/serve-static-files-python-3-flask/
+# Using flask with tor: source: https://gist.github.com/PaulSec/ec8f1689bfde3ce9a920
+
+# Set tor_cmd: source: https://stackoverflow.com/a/25069013
+WEB_PORT = 8080
+CONTROL_PORT = 7001
+SOCKS_PORT = 7000
+HIDDEN_SERVICE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'hidden_service')
+static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'public')
+TOR_CMD = "/Applications/TorBrowser.app/Tor/tor.real"
+
+#app = Flask(__name__)
+
+
+# //TODO: check password strength with zxcvbn
+class KeySwarmSession(object):
+    def __init__(self, email, password):
+        salt = ('KeySwarm Profile Signature Key:'.join(email)).encode('utf-8')
+        pw = password.encode('utf-8')
+        hashed_pw = pyblake2.blake2s(pw).digest()
+        seed = pylibscrypt.scrypt(hashed_pw, salt, 2**17, 8, 1, 32)
+        self.private_key = ed25519.SigningKey(seed)
+        self.public_key = self.private_key.get_verifying_key()
+        self.address = base58.b58encode( self.public_key.to_bytes() + pyblake2.blake2s(self.public_key.to_bytes(), 1).digest() )
+        print((self.address).decode('utf-8'))
+
+ss = KeySwarmSession("test@example.com", "123455678999")
+
+class ProfileSigner(object):
+    def __init__(self, ks_session, path):
+        self.ks_session = ks_session
+        self.path = path
+
+    def sign(self):
+        # //TODO: use this function to walk dir
+        # https://www.bogotobogo.com/python/python_traversing_directory_tree_recursively_os_walk.php
+        path = "./TEST"
+        fname = []
+        for root,d_names,f_names in os.walk(path):
+        	for f in f_names:
+        		fname.append(os.path.join(root, f))
+
+        print("fname = %s" %fname)
+
+
+    def sign_file(self, path):
+        pass
+
+class EncMetaInfo(object):
+    def __init__(self):
+        self.content = ""
+        self.signature = ""
+
+class MetaInfoContent(object):
+    def __init__(self):
+        self.version = 0
+        self.address = ""
+        self.files = []
+        self.timestamp = 0
+
+class MetaInfoFile(object):
+    def __init__(self):
+        self.path = ""
+        self.hash = ""
+
+
+"""
+def start_web_app():
+    print ('Starting web app')
+    app.run(port=WEB_PORT, threaded=True)
+
+def print_bootstrap_lines(line):
+    if "Bootstrapped " in line:
+        print(term.format(line, term.Color.BLUE))
+
+@app.route('/', methods=['GET'])
+def serve_dir_directory_index():
+    return send_from_directory(static_file_dir, 'index.html')
+
+
+@app.route('/<path:path>', methods=['GET'])
+def serve_file_in_dir(path):
+
+    if not os.path.isfile(os.path.join(static_file_dir, path)):
+        path = os.path.join(path, 'index.html')
+
+    return send_from_directory(static_file_dir, path)
+
+
+def main():
+    print(term.format("Starting Tor:\n", term.Attr.BOLD))
+
+    tor_process = stem.process.launch_tor_with_config(
+      config = {
+        'tor_cmd': TOR_CMD,
+        'SocksPort': str(SOCKS_PORT),
+        'ControlPort': str(CONTROL_PORT),
+        'ExitNodes': '{ru}',
+      },
+      init_msg_handler = print_bootstrap_lines,
+    )
+
+    # Start the flask web app in a separate thread
+    t = threading.Thread(target=start_web_app)
+    t.daemon = True
+    t.start()
+
+    # Connect to the Tor control port
+    try:
+        c = Controller.from_port(port=CONTROL_PORT)
+        c.authenticate()
+    except SocketError:
+        print ('Cannot connect to Tor control port')
+        sys.exit()
+
+    # Create an ephemeral hidden service
+    try:
+        print ('Creating hidden service')
+        result = c.create_hidden_service(HIDDEN_SERVICE_DIR, 80, target_port=WEB_PORT)
+        print (" * Created host: %s" % result.hostname)
+        onion = result.hostname
+    except UnsatisfiableRequest:
+        print ('Cannot create ephemeral hidden service, Tor version is too old')
+        sys.exit()
+    except Exception, e:
+        print e
+        sys.exit()
+
+    t.join()
+if __name__ == '__main__':
+main()
+"""
